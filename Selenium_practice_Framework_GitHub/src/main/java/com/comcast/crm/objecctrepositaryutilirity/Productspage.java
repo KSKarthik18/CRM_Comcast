04b6ab75d54699040a8caf7779989689c2e989db
Admin <Admin@DESKTOP-UQJIA5A.mshome.net>
2025-02-26 11:16:10 +0530

    Product added

--- Selenium_practice_Framework_GitHub/src/main/java/com/comcast/crm/objecctrepositaryutilirity/Productspage.java
+++ Selenium_practice_Framework_GitHub/src/main/java/com/comcast/crm/objecctrepositaryutilirity/Productspage.java
@@ -1,9 +1,15 @@
 package com.comcast.crm.objecctrepositaryutilirity;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Productspage {
 	
 	
 	//Hi karthik
 	
 	//Hi Luffy
+	
+	@FindBy(xpath = "Name")
+	private WebElement ele1;
 }
